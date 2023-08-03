7e160165e4ac19a2314a340ad94b269467596d4c
Hansen <hansenchan1995@gmail.com>
2023-08-03 14:34:59 +0800

    added market data

--- marketDataService.py
+++ marketDataService.py
@@ -17,6 +17,16 @@ class MarketDataService:
         print("[%d]<<<<< call MarketDataService.init" % (os.getpid(),))
         time.sleep(3)
         self.produce_market_data(marketData_2_exchSim_q, marketData_2_platform_q)
+        self.stock_midquote = {3443: 124905.336,
+                                 2388: 8040.317,
+                                 2498: 6002.225,
+                                 2610: 2375.1636,
+                                 1319: 5017.527,
+                                 3035: 18295.475,
+                                 3006: 8452.317,
+                                 2615: 6256.2876,
+                                 5425: 9768.326,
+                                 3105: 16809.557}
 
     def produce_market_data(self, marketData_2_exchSim_q, marketData_2_platform_q):
         for i in range(200):
@@ -24,37 +34,21 @@ class MarketDataService:
             time.sleep(1)
 
     def produce_quote(self, marketData_2_exchSim_q, marketData_2_platform_q):
-        #TODO: read from file
-        bidPrice, askPrice, bidSize, askSize = [], [], [], []
-        bidPrice1 = 20+random.randint(0,100)/10
-        askPrice1 = bidPrice1 + 0.01
-        for i in range(5):
-            bidPrice.append(bidPrice1-i*0.01)
-            askPrice.append(askPrice1+i*0.01)
-            bidSize.append(100+random.randint(0,100)*100)
-            askSize.append(100+random.randint(0,100)*100)
-        quoteSnapshot = OrderBookSnapshot_FiveLevels('testTicker_1', datetime.datetime.now().strftime('%Y-%m-%d'), datetime.datetime.now(),bidPrice, askPrice, bidSize, askSize)
-        print('[%d]MarketDataService>>>produce_quote' % (os.getpid()))
-        print(quoteSnapshot.outputAsDataFrame())
-        marketData_2_exchSim_q.put(quoteSnapshot)
-        time.sleep(2)
-        marketData_2_platform_q.put(quoteSnapshot)
-        time.sleep(2)
+        for stock_ticker, midquote in self.stock_midquote:
+            bidPrice, askPrice, bidSize, askSize = [], [], [], []
+            bidPrice1 = midquote + random.randint(0, 100) / 10
+            askPrice1 = bidPrice1 + 0.01
+            for i in range(5):
+                bidPrice.append(bidPrice1 - i * 0.01)
+                askPrice.append(askPrice1 + i * 0.01)
+                bidSize.append(midquote + random.randint(0, 100) * 100)
+                askSize.append(midquote + random.randint(0, 100) * 100)
+            quoteSnapshot = OrderBookSnapshot_FiveLevels(str(stock_ticker), datetime.datetime.now().strftime('%Y-%m-%d'),
+                                                         datetime.datetime.now(), bidPrice, askPrice, bidSize, askSize)
+            print('[%d]MarketDataService>>>produce_quote' % (os.getpid()))
+            print(quoteSnapshot.outputAsDataFrame())
+            marketData_2_exchSim_q.put(quoteSnapshot)
+            time.sleep(2)
+            marketData_2_platform_q.put(quoteSnapshot)
+            time.sleep(2)
 
-
-
-        bidPrice, askPrice, bidSize, askSize = [], [], [], []
-        bidPrice1 = 100+random.randint(0,100)/10
-        askPrice1 = bidPrice1 + 0.01
-        for i in range(5):
-            bidPrice.append(bidPrice1-i*0.01)
-            askPrice.append(askPrice1+i*0.01)
-            bidSize.append(100+random.randint(0,100)*100)
-            askSize.append(100+random.randint(0,100)*100)
-        quoteSnapshot = OrderBookSnapshot_FiveLevels('testTicker_2', datetime.datetime.now().strftime('%Y-%m-%d'), datetime.datetime.now(),bidPrice, askPrice, bidSize, askSize)
-        print('[%d]MarketDataService>>>produce_quote' % (os.getpid()))
-        print(quoteSnapshot.outputAsDataFrame())
-        marketData_2_exchSim_q.put(quoteSnapshot)
-        time.sleep(2)
-        marketData_2_platform_q.put(quoteSnapshot)
-        time.sleep(2)
